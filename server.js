5caaf8981c5f519afc1b90f71552f5894c68b795
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-05-10 16:05:21 +0200

    process environment

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv');
+// Load env vars: read the .env file and assign the values to process.env
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-// 4) START THE SERVER
-const port = 3000;
+console.log(process.env.NODE_ENV);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
